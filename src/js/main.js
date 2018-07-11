fb7e98aa371cadac756c2890cad4dac6690f3d41
vbiaggi10 <vbiaggi10@gmail.com>
2018-07-11 13:24:23 -0500

    practice

--- src/js/main.js
+++ src/js/main.js
@@ -60,3 +60,14 @@
  } */
 
 })(jQuery);
+
+document.querySelector('#log-out').addEventListener('click', (e) => {
+  console.log('holaaaa')
+  firebase.auth().signOut().then( function(){
+    if (e.preventDefault) {
+      window.location.assign('signin.html')
+    }
+  }).catch( function(error){
+
+  });
+})
