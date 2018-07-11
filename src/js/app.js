fba0aa5a772c89d06890119199bae7d98b8f8d91
vbiaggi10 <vbiaggi10@gmail.com>
2018-07-11 14:19:42 -0500

    corrigiendo errores

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ const createUser = (email, password, repeatPassword) => {
     console.log('Your password doesnt mach');
     alert('Your password doesnt mach');
   }
-}
+};
 
 const signInUser = (email, password) => {
   firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
@@ -36,7 +36,7 @@ const signInUser = (email, password) => {
       }
       console.log(error);
     });
-}
+};
 
 const resetPassword = (email, password) => {
   firebase.auth().sendPasswordResetEmail(
@@ -56,7 +56,7 @@ const resetPassword = (email, password) => {
       }
       console.log(error);
     });
-}
+};
 
 const loginWithGoogle = () => {
   // Using a popup.
@@ -70,7 +70,7 @@ const loginWithGoogle = () => {
     var user = result.user;
     window.location.assign('main.html')
   });
-}
+};
 
 const loginWithFacebook = () => {
   // Sign in using a popup.
@@ -83,7 +83,7 @@ const loginWithFacebook = () => {
     var user = result.user;
     window.location.assign('main.html')
   });
-}
+};
 
 const loginWithTwitter = () => {
   // Using a popup.
@@ -96,4 +96,67 @@ const loginWithTwitter = () => {
     var user = result.user;
     window.location.assign('main.html')
   });
-}
+};
+
+(function ($) {
+  "use strict";
+  //  [ Focus input ]
+  $('.input100').each(function () {
+    $(this).on('blur', function () {
+      if ($(this).val().trim() != "") {
+        $(this).addClass('has-val');
+      }
+      else {
+        $(this).removeClass('has-val');
+      }
+    })
+  })
+
+/* // [ Validate ]
+ var input = $('.validate-input .input100');
+ 
+ $('.validate-form').on('submit', function () {
+   var check = true;
+ 
+   for (var i = 0; i < input.length; i++) {
+     if (validate(input[i]) == false) {
+       showValidate(input[i]);
+       check = false;
+     }
+   }
+   return check;
+ });
+ 
+ 
+ $('.validate-form .input100').each(function () {
+   $(this).focus(function () {
+     hideValidate(this);
+   });
+ });
+ 
+ function validate(input) {
+   if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+     if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+       return false;
+     }
+   }
+   else {
+     if ($(input).val().trim() == '') {
+       return false;
+     }
+   }
+ }
+ 
+ function showValidate(input) {
+   var thisAlert = $(input).parent();
+ 
+   $(thisAlert).addClass('alert-validate');
+ }
+ 
+ function hideValidate(input) {
+   var thisAlert = $(input).parent();
+ 
+   $(thisAlert).removeClass('alert-validate');
+ } */
+
+})(jQuery);
