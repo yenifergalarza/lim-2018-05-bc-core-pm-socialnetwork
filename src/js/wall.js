4a694ac7c52002ad9543a844166a4fc4f6e47187
unknown <natalypc27@gmail.com>
2018-07-16 11:43:31 -0500

    cambios de posts

--- src/js/wall.js
+++ src/js/wall.js
@@ -1,5 +1,4 @@
 document.querySelector('#log-out').addEventListener('click', (e) => {
-  console.log('holaaaa')
   firebase.auth().signOut().then(function () {
     if (e.preventDefault) {
       window.location.assign('index.html')
