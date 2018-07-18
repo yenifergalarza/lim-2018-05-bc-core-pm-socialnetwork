8224a4f9bb2c4db96f286e241dd3ff0950d7ea93
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-07-18 17:50:33 -0500

    subiendo cambios

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
-const createUser = (email, password, repeatPassword) => {
+window.createUser = (email, password, repeatPassword) => {
   if (password === repeatPassword) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(() => {
-        window.location.assign('signin.html')
+        window.location.assign('index.html')
       })
       .catch(function (error) {
         // Handle Errors here.
@@ -21,10 +21,10 @@ const createUser = (email, password, repeatPassword) => {
   }
 };
 
-const signInUser = (email, password) => {
+window.signInUser = (email, password) => {
   firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-    window.location.assign('main.html')
-  })
+      window.location.assign('main.html')
+    })
     .catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -38,9 +38,9 @@ const signInUser = (email, password) => {
     });
 };
 
-const resetPassword = (email, password) => {
+window.resetPassword = (email, password) => {
   firebase.auth().sendPasswordResetEmail(
-    getEmail.value)
+      getEmail.value)
     .then(function () {
       firebase.auth.EmailAuthProvider.credential(email, password);
       alert("Password Reset Email Sent")
@@ -57,22 +57,26 @@ const resetPassword = (email, password) => {
       console.log(error);
     });
 };
-
-const loginWithGoogle = () => {
+window.writeUserData = (userId, name, email, imageUrl) => {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+};
+window.loginWithGoogle = () => {
   // Using a popup.
   var provider = new firebase.auth.GoogleAuthProvider();
   provider.addScope('profile');
   provider.addScope('email');
   firebase.auth().signInWithPopup(provider).then(function (result) {
-    // This gives you a Google Access Token.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
     var user = result.user;
+    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
     window.location.assign('main.html')
   });
 };
 
-const loginWithFacebook = () => {
+window.loginWithFacebook = () => {
   // Sign in using a popup.
   var provider = new firebase.auth.FacebookAuthProvider();
   provider.addScope('user_birthday');
@@ -85,7 +89,7 @@ const loginWithFacebook = () => {
   });
 };
 
-const loginWithTwitter = () => {
+window.loginWithTwitter = () => {
   // Using a popup.
   var provider = new firebase.auth.TwitterAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function (result) {
@@ -98,65 +102,69 @@ const loginWithTwitter = () => {
   });
 };
 
-(function ($) {
-  "use strict";
-  //  [ Focus input ]
-  $('.input100').each(function () {
-    $(this).on('blur', function () {
-      if ($(this).val().trim() != "") {
-        $(this).addClass('has-val');
-      }
-      else {
-        $(this).removeClass('has-val');
-      }
-    })
-  })
 
-/* // [ Validate ]
- var input = $('.validate-input .input100');
- 
- $('.validate-form').on('submit', function () {
-   var check = true;
- 
-   for (var i = 0; i < input.length; i++) {
-     if (validate(input[i]) == false) {
-       showValidate(input[i]);
-       check = false;
-     }
-   }
-   return check;
- });
- 
- 
- $('.validate-form .input100').each(function () {
-   $(this).focus(function () {
-     hideValidate(this);
-   });
- });
- 
- function validate(input) {
-   if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-     if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-       return false;
-     }
-   }
-   else {
-     if ($(input).val().trim() == '') {
-       return false;
-     }
-   }
- }
- 
- function showValidate(input) {
-   var thisAlert = $(input).parent();
- 
-   $(thisAlert).addClass('alert-validate');
- }
- 
- function hideValidate(input) {
-   var thisAlert = $(input).parent();
- 
-   $(thisAlert).removeClass('alert-validate');
- } */
+window.writeUserData = (userId, name, email, imageUrl) => {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture: imageUrl
+  });
+};
 
-})(jQuery);
+window.writeNewPost = (uid, body,countlike) => {
+  // A post entry.
+  var postData = {
+    body: body,
+    countlike:countlike,
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  firebase.database().ref().update(updates);
+  return newPostKey;
+};
+
+// window.loadPost = (userId) => {
+// const post = document.getElementById("loadedPost");
+// let userId = 'lOAgxhLUxpXUlFwEiCOD7PfG8iz2';
+// var dbRefPost = firebase.database().ref().child('user-posts');
+// var dbRefPostUser = dbRefPost.child(userId);
+// dbRefPostUser.on('child_added', snap => {
+//   snap.forEach(body => {
+//     console.log(body.val());
+//     const buttonUpdate = document.createElement("input");
+//     buttonUpdate.setAttribute("value", "Update");
+//     buttonUpdate.setAttribute("type", "button");
+//     const buttonDelete = document.createElement("input");
+//     buttonDelete.setAttribute("value", "Delete");
+//     buttonDelete.setAttribute("type", "button");
+//     const buttonLike = document.createElement("input");
+//     buttonLike.setAttribute("value", "Me gusta");
+//     buttonLike.setAttribute("type", "button");
+//     const contenidoPost = document.createElement('div');
+//     contenidoPost.className = 'contenidoPost';
+//     const textoPost = document.createElement('textarea')
+//     textoPost.innerHTML = body.val();
+//     textoPost.disabled = true;
+//     post.appendChild(textoPost);
+//     post.appendChild(buttonUpdate);
+//     post.appendChild(buttonDelete);
+//     post.appendChild(buttonLike);
+
+//   });
+// });
+
+// dbRefPostUser.on('child_added', snap => post.innerHTML = JSON.stringify(snap.val()));
+
+/* return firebase.database().ref('/user-posts/' + userId ).once('value').then(function (snapshot) {
+  var body = (snapshot.val() && snapshot.val().body) || 'Anonymous';
+  console.log(body);
+  // ...
+}); */
+// }
--- src/js/signin.js
+++ src/js/signin.js
@@ -5,8 +5,18 @@ const logIn = document.querySelector("#log-In")
 const loginGoogle = document.querySelector('#login-google');
 const loginFacebook = document.querySelector('#login-facebook');
 const loginTwitter = document.querySelector('#login-twitter');
+const username = document.querySelector("#text-white");
 const forgotPassword = document.querySelector('#forgot-Password');
-
+window.onload =()=>{
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      window.location.assign('wall.html')
+    } else {
+      console.log('No user is signed in.');
+      
+    }
+  });
+}
 register.addEventListener('click', (e) => {
   if (e.preventDefault) {
     window.location.assign('signup.html')
