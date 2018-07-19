1c01cf5e21bdfe9b5c51f1c333634f081bf837b2
vbiaggi10 <vbiaggi10@gmail.com>
2018-07-19 12:35:38 -0500

    refactorizando

--- src/js/wall.js
+++ src/js/wall.js
@@ -37,70 +37,21 @@ buttonPublish.addEventListener('click', () => {
 });
 firebase.auth().onAuthStateChanged(function (user) {
   if (firebase.auth().currentUser.isAnonymous === true) {
-    document.querySelector('.create-post').style.display = 'none';
-    document.querySelector('.profile-card').style.display = 'none';
-    // window.onload = () => {
-      // firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
+
+    if (user) {
       const dbRefPost = firebase.database().ref().child('posts');
 
-      const hola2 = (postKey) => {
-        postKey.forEach(keys => {
-          let postId = keys.key;
-          console.log(keys.val().body)
-          const buttonLike = document.createElement('i');
-          buttonLike.setAttribute('data-postId', postId);
-          buttonLike.setAttribute('class', 'far fa-heart post-icon');
-          const cantidadLikes = document.createElement("span");
-          const contenidoPost = document.createElement('div');
-          contenidoPost.className = 'contenidoPost';
-          const textoPost = document.createElement('textarea')
-          textoPost.setAttribute('id', postId);
-          textoPost.setAttribute('class', 'form-control');
-          textoPost.innerHTML = keys.val().body;
-          cantidadLikes.innerHTML = keys.val().countLike;
-          textoPost.disabled = true;
-          buttonLike.classList.add('btn-like');
-
-          buttonLike.addEventListener('click', () => {
-            count_click += 1;
-            cantidadLikes.innerHTML = "A " + count_click + " le gustan este post";
-
-            const nuevoPost = {
-              body: keys.val().body,
-              countLike: count_click,
-            };
-
-            const updatesUser = {};
-            const updatesPost = {};
-
-            updatesUser['/user-posts/' + userId + '/' + postId] = nuevoPost;
-            updatesPost['/posts/' + postId] = nuevoPost;
-            firebase.database().ref().update(updatesUser);
-            firebase.database().ref().update(updatesPost);
-          });
-
-          contenidoPost.appendChild(textoPost);
-          contenidoPost.appendChild(buttonLike);
-          contenidoPost.appendChild(cantidadLikes);
-          posts.appendChild(contenidoPost);
-
-        })
-      }
       dbRefPost.once('value', postKey => {
-        hola2(postKey);
-        console.log(postKey);
-        // console.log(document.querySelectorAll('.btn-delete'))
-
-
+        paintPost(postKey);
+        // console.log(postKey);
+        // document.querySelector('data-postid').style.display = 'none';
+        // $('.post-icon').addClass('unshow');
       })
-      // }
-      // })
     }
   } else {
-    // window.onload = () => {
-      // firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
+    document.querySelector('.create-post').style.display = 'block';
+    document.querySelector('.profile-card').style.display = 'block';
+    if (user) {
       profile.innerHTML = `<img src="${user.photoURL}" alt="user" class="profile-photo" />
                               <h5>
                                 <a href="timeline.html" id="name"class="text-white">${user.displayName}</a>
@@ -110,83 +61,11 @@ firebase.auth().onAuthStateChanged(function (user) {
       let userId = firebase.auth().currentUser.uid;
       const dbRefPost = firebase.database().ref().child('user-posts').child(userId);
 
-      const hola2 = (postKey) => {
-        postKey.forEach(keys => {
-          let postId = keys.key;
-          console.log(keys.val().body)
-          const buttonUpdate = document.createElement('i');
-          buttonUpdate.setAttribute('class', 'far fa-edit post-icon');
-          const buttonDelete = document.createElement('i');
-          buttonDelete.setAttribute('data-postId', postId);
-          buttonDelete.setAttribute('class', 'far fa-trash-alt post-icon');
-          const buttonLike = document.createElement('i');
-          buttonLike.setAttribute('data-postId', postId);
-          buttonLike.setAttribute('class', 'far fa-heart post-icon');
-          const cantidadLikes = document.createElement("span");
-          const contenidoPost = document.createElement('div');
-          contenidoPost.className = 'contenidoPost';
-          const textoPost = document.createElement('textarea')
-          textoPost.setAttribute('id', postId);
-          textoPost.setAttribute('class', 'form-control');
-          textoPost.innerHTML = keys.val().body;
-          cantidadLikes.innerHTML = keys.val().countLike;
-          textoPost.disabled = true;
-
-          buttonDelete.classList.add('btn-delete');
-          buttonLike.classList.add('btn-like');
-
-          let aux = 0;
-          buttonUpdate.addEventListener('click', () => {
-            if (aux === 0) {
-              textoPost.disabled = false;
-              aux = 1;
-            } else {
-              textoPost.disabled = true;
-              aux = 0;
-            }
-            const newUpdate = document.getElementById(postId);
-            const nuevoPost = {
-              body: newUpdate.value,
-            };
-
-            const updatesUser = {};
-            const updatesPost = {};
-
-            updatesUser['/user-posts/' + userId + '/' + postId] = nuevoPost;
-            /*       updatesPost['/posts/' + newPost] = nuevoPost; */
-            firebase.database().ref().update(updatesUser);
-            firebase.database().ref().update(updatesPost);
-          });
-
-          buttonLike.addEventListener('click', () => {
-            count_click += 1;
-            cantidadLikes.innerHTML = "A " + count_click + " le gustan este post";
-
-            const nuevoPost = {
-              body: keys.val().body,
-              countLike: count_click,
-            };
-
-            const updatesUser = {};
-            const updatesPost = {};
-
-            updatesUser['/user-posts/' + userId + '/' + postId] = nuevoPost;
-            updatesPost['/posts/' + postId] = nuevoPost;
-            firebase.database().ref().update(updatesUser);
-            firebase.database().ref().update(updatesPost);
-          });
-
-          contenidoPost.appendChild(textoPost);
-          contenidoPost.appendChild(buttonUpdate);
-          contenidoPost.appendChild(buttonDelete);
-          contenidoPost.appendChild(buttonLike);
-          contenidoPost.appendChild(cantidadLikes);
-          posts.appendChild(contenidoPost);
-
-        })
-      }
       dbRefPost.once('value', postKey => {
-        hola2(postKey);
+        paintPost(postKey);
+        const displayIcon = document.querySelectorAll('.contenidoPost .post-icon');
+        displayIcon.style.display = 'inline-block';
+
         // console.log(document.querySelectorAll('.btn-delete'))
         const buttonsDelete = document.querySelectorAll('.btn-delete');
         buttonsDelete.forEach(button => {
@@ -199,15 +78,92 @@ firebase.auth().onAuthStateChanged(function (user) {
 
             alert('The user is deleted successfully!');
             dbRefPost.on('value', postKey => {
-              hola2(postKey)
+              paintPost(postKey)
+              document.querySelector('.post-icon').style.display = 'block';
+
             })
             reload_page();
           });
         })
 
       })
-      }
-      // })
-    // }
+    }
   }
 })
+
+const paintPost = (postKey) => {
+  postKey.forEach(keys => {
+    let postId = keys.key;
+    console.log(keys.val().body)
+    const buttonUpdate = document.createElement('i');
+    buttonUpdate.setAttribute('class', 'far fa-edit post-icon');
+    buttonUpdate.setAttribute('data-postId', postId);
+    const buttonDelete = document.createElement('i');
+    buttonDelete.setAttribute('data-postId', postId);
+    buttonDelete.setAttribute('class', 'far fa-trash-alt post-icon');
+    const buttonLike = document.createElement('i');
+    buttonLike.setAttribute('data-postId', postId);
+    buttonLike.setAttribute('class', 'far fa-heart post-icon');
+    const cantidadLikes = document.createElement("span");
+    const contenidoPost = document.createElement('div');
+    contenidoPost.className = 'contenidoPost';
+    const textoPost = document.createElement('textarea')
+    textoPost.setAttribute('id', postId);
+    textoPost.setAttribute('class', 'form-control');
+    textoPost.innerHTML = keys.val().body;
+    cantidadLikes.innerHTML = keys.val().countLike;
+    textoPost.disabled = true;
+
+    buttonDelete.classList.add('btn-delete');
+    buttonLike.classList.add('btn-like');
+
+    let aux = 0;
+    buttonUpdate.addEventListener('click', () => {
+      if (aux === 0) {
+        textoPost.disabled = false;
+        aux = 1;
+      } else {
+        textoPost.disabled = true;
+        aux = 0;
+      }
+      const newUpdate = document.getElementById(postId);
+      const nuevoPost = {
+        body: newUpdate.value,
+      };
+
+      const updatesUser = {};
+      const updatesPost = {};
+
+      updatesUser['/user-posts/' + userId + '/' + postId] = nuevoPost;
+      /*       updatesPost['/posts/' + newPost] = nuevoPost; */
+      firebase.database().ref().update(updatesUser);
+      firebase.database().ref().update(updatesPost);
+    });
+
+    buttonLike.addEventListener('click', () => {
+      count_click += 1;
+      cantidadLikes.innerHTML = "A " + count_click + " le gustan este post";
+
+      const nuevoPost = {
+        body: keys.val().body,
+        countLike: count_click,
+      };
+
+      const updatesUser = {};
+      const updatesPost = {};
+
+      updatesUser['/user-posts/' + userId + '/' + postId] = nuevoPost;
+      updatesPost['/posts/' + postId] = nuevoPost;
+      firebase.database().ref().update(updatesUser);
+      firebase.database().ref().update(updatesPost);
+    });
+
+    contenidoPost.appendChild(textoPost);
+    contenidoPost.appendChild(buttonUpdate);
+    contenidoPost.appendChild(buttonDelete);
+    contenidoPost.appendChild(buttonLike);
+    contenidoPost.appendChild(cantidadLikes);
+    posts.appendChild(contenidoPost);
+
+  })
+}
