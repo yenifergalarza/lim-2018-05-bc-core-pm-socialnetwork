7b41611e76738257a02caef3b5e0ad00a2a4e2c6
unknown <natalypc27@gmail.com>
2018-07-29 00:08:31 -0500

    cambio general

--- test/headless.js
+++ test/headless.js
@@ -1,14 +1,6 @@
 global.window = global;
 global.assert = require('chai').assert;
-<<<<<<< HEAD
 global.jestExpect = global.expect;
-=======
-/* global.fixtures = {
-  cohorts: require('../data/cohorts.json'),
-  progress: require('../data/cohorts/lim-2018-03-pre-core-pw/progress.json'),
-  users: require('../data/cohorts/lim-2018-03-pre-core-pw/users.json'),
-}; */
->>>>>>> 343cb40f1e25dcdf767d8a8388f8a6e64b938390
 require('../src/js/app');
 const firebaseMock = require('firebase-mock')
 global.firebase = firebaseMock.MockFirebaseSdk()
