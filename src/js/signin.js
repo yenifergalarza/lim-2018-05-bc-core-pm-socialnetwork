d1a1e66d4a6feba355b5a5796b08ec592612e932
yenifergalarza <yeniferalexandra2000@hotmail.com>
2018-07-30 14:20:33 -0500

    bien

--- src/js/signin.js
+++ src/js/signin.js
@@ -7,15 +7,6 @@ const loginFacebook = document.querySelector('#login-facebook');
 const loginTwitter = document.querySelector('#login-twitter');
 const username = document.querySelector("#text-white");
 const forgotPassword = document.querySelector('#forgot-Password');
-<<<<<<< HEAD
-window.onload =()=>{
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      window.location.assign('wall.html')
-    } else {
-      console.log('No user is signed in.');
-      
-=======
 const loginAnonymous = document.querySelector('#login-anonymous');
 
 
@@ -28,7 +19,6 @@ window.onload = () => {
     } else {
       console.log('No user is signed in.');
 
->>>>>>> 34facf06f23b3f9530b4b7abf5ab63e4c8e4964b
     }
   });
 }
